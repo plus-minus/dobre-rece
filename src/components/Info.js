589da23a18ba243afc5f1dfec5ed3c7e194bbb8d
plus-minus <blue.box@o2.pl>
2019-10-15 17:21:02 +0200

    Created Info component. JS&SCSS

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,7 +3,23 @@ import "../scss/Info.scss";
 
 function Info() {
     return (
-        <section id="home-info"></section>
+        <section id="home-info" className="center-content">
+            <div>
+                <span>10</span>
+                <span>oddanych worków</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </div>
+            <div>
+                <span>5</span>
+                <span>wspartych organizacji</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </div>
+            <div>
+                <span>7</span>
+                <span>zorganizowanych zbiórek</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </div>
+        </section>
     );
 }
 
