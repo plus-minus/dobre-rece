a77a65718bb8995b75a96b5e16dc307b6af10343
plus-minus <blue.box@o2.pl>
2019-10-16 12:20:55 +0200

    Added SimpleSteps/About section

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,14 @@ import "../scss/About.scss";
 
 function About() {
     return (
-        <section id="home-about"></section>
+        <section id="home-about">
+            <div className="about-info">
+                <h1>O nas</h1>
+                <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                <span className="signature"></span>
+            </div>
+            <div className="about-img"></div>
+        </section>
     );
 }
 
--- src/components/SimpleSteps.js
+++ src/components/SimpleSteps.js
@@ -1,9 +1,36 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import "../scss/SimpleSteps.scss";
 
 function SimpleSteps() {
     return (
-        <section id="home-simpleSteps"></section>
+        <section id="home-simpleSteps">
+            <h1>Wystarczą 4 proste kroki</h1>
+            <article className="steps">
+                <div className="step">
+                    <div className="step-img shirt"></div>
+                    <p className="step-desc">Wybierz rzeczy</p>
+                    <p className="step-tip">ubrania, zabawki, sprzęt i inne</p>
+                </div>
+                <div className="step">
+                    <div className="step-img bag"></div>
+                    <p className="step-desc">Spakuj je</p>
+                    <p className="step-tip">skorzystaj z worków na śmieci</p>
+                </div>
+                <div className="step">
+                    <div className="step-img zoom"></div>
+                    <p className="step-desc">Zdecyduj komu chcesz pomóc</p>
+                    <p className="step-tip">wybierz zaufane miejsce</p>
+                </div>
+                <div className="step">
+                    <div className="step-img order"></div>
+                    <p className="step-desc">Zamów kuriera</p>
+                    <p className="step-tip">kurier przyjedzie w dogodnym terminie</p>
+                </div>
+
+            </article>
+            <Link to="/oddaj-rzeczy">Oddaj<br/>rzeczy</Link>
+        </section>
     );
 }
 
