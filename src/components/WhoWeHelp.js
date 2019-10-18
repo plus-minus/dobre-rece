9407559ae6f0f15aa9c4b1e80348b863b781cb4f
plus-minus <blue.box@o2.pl>
2019-10-18 16:23:48 +0200

    Added fetching from JSON server

--- src/components/WhoWeHelp.js
+++ src/components/WhoWeHelp.js
@@ -5,78 +5,102 @@ class WhoWeHelp extends React.Component {
     constructor() {
         super();
         this.state = {
-            selected: "loc" //fou/org/loc
+            selected: "loc", //fou/org/loc
+            founds: [],
+            orgs: [],
+            local:[]
         };
 
         this.changeTab = this.changeTab.bind(this);
     }
 
-    componentDidMount(){
+    componentDidMount() {
 
-        fetch("/foundations", {
+        fetch("http://localhost:3001/foundations", {
             method: "GET",
             headers: {
-                "Content-Type" : "application/json"
+                "Content-Type": "application/json"
             }
         })
-        .then(res => res.json())
-        .then(json => console.log(json))
-        .catch(err => console.error(err))
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    founds: json
+                })
+            })
+            .catch(err => console.error(err))
 
     }
 
-    changeTab(e){
+    changeTab(e) {
         const el = e.target;
         this.setState({
             selected: el.id
         });
-
-
     }
+
+    createContent(array){
+        return (
+            <div className="tab-content">
+                <p>W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.</p>
+                <ul className="foundations">
+                    {
+                        array.length > 0 ?
+                            array.map((f, i) => {
+                                return <><li key={i} className="foundation">
+                                    <div>
+                                        <h3 className="fundName">Fundacja {f.nazwa}</h3>
+                                        <p className="goal">Cel i misja: {f.cel}</p>
+                                    </div>
+                                    <div className="What">{f.dary.join(", ")}</div>
+
+                                </li>
+                                <hr /></>
+                            }).slice(0, 3)
+                            :
+                            <h3>Nie znaleziono </h3>
+                    }
+                    </ul>
+                </div>
+        );
+    }
+
+
     render() {
+        const { founds, selected, orgs, local } = this.state;
+        let content;
+
+        switch(selected){
+            case "fou":
+                content = this.createContent(founds);
+                break;
+            case "org":
+                content = this.createContent(orgs);
+                break;
+            case "loc":
+                content = this.createContent(local);
+                break;
+            default: 
+                content = <h3>Coś poszło nie tak...</h3>
+        }
+
         return (
             <section id="home-whoWeHelp">
                 <h1>Komu pomagamy?</h1>
                 <ul className="tabs">
-                    <li id="fou" className={this.state.selected === "fou"? "tab selected" : "tab"}
-                    onClick={this.changeTab}
+                    <li  id="fou" className={this.state.selected === "fou" ? "tab selected" : "tab"}
+                        onClick={this.changeTab}
                     >Fundacjom</li>
                     <li id="org" className={this.state.selected === "org" ? "tab selected" : "tab"}
                         onClick={this.changeTab}
-                        >Organizacjom pozarządowym</li>
-                    <li  id="loc" className={this.state.selected === "loc" ? "tab selected" : "tab"}
+                    >Organizacjom pozarządowym</li>
+                    <li id="loc" className={this.state.selected === "loc" ? "tab selected" : "tab"}
                         onClick={this.changeTab}
-                        >Lokalnym zbiórkom</li>
+                    >Lokalnym zbiórkom</li>
                 </ul>
-                <div className="tab-content">
-                    <p>W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.</p>
-                    <ul className="foundations">
-                        <li className="foundation">
-                            <div>
-                                <h3 className="fundName">Fundacja “Dbam o Zdrowie”</h3>
-                                <p className="goal">Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.</p>
-                            </div>
-                            <div className="What">ubrania, jedzenie, sprzęt AGD, meble, zabawki</div>
-                        </li>
-                        <hr />
-                        <li className="foundation">
-                            <div>
-                                <h3 className="fundName">Fundacja “Dla dzieci”</h3>
-                                <p className="goal">Cel i misja: Pomoc dzieciom z ubogich rodzin.</p>
-                            </div>
-                            <div className="What">ubrania, meble, zabawki</div>
-                        </li>
-                        <hr />
-                        <li className="foundation">
-                            <div>
-                                <h3 className="fundName">Fundacja “Bez domu”</h3>
-                                <p className="goal">Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.</p>
-                            </div>
-                            <div className="What">ubrania, jedzenie, ciepłe koce</div>
-                        </li>
-                        <hr />
-                    </ul>
-                </div>
+
+                {content}
+
             </section>
         );
     }
